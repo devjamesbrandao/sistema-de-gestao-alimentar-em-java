fb6d091ff9fb42b4fa692174660cbe7a78c0da18
James <56309987+devjamesbrandao@users.noreply.github.com>
2024-06-19 14:07:09 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,26 +1,4 @@
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
-
-class Sistema {
-    private Map<String, Produto> bancoDeProdutos;
-
-    public Sistema() {
-        bancoDeProdutos = new HashMap<>();
-        inicializarProdutos();
-    }
-
-    private void inicializarProdutos() {
-        // Adicionando alguns produtos ao banco de dados
-        bancoDeProdutos.put("1234567890123", new Produto("1234567890123", "Biscoito Recheado", "Farinha de trigo, açúcar, óleo vegetal, cacau, leite em pó, fermento químico, emulsificante"));
-        bancoDeProdutos.put("9876543210987", new Produto("9876543210987", "Leite Integral", "Leite integral"));
-        bancoDeProdutos.put("1928374650912", new Produto("1928374650912", "Suco de Laranja", "Suco de laranja concentrado, água, açúcar"));
-    }
-
-    public Produto buscarProdutoPorCodigo(String codigoDeBarras) {
-        return bancoDeProdutos.get(codigoDeBarras);
-    }
-}
 
 public class Main {
     public static void main(String[] args) {
