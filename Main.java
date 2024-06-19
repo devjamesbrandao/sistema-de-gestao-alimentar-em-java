31d514fc5fbdc961b63271ac5050a51537e6589e
James <56309987+devjamesbrandao@users.noreply.github.com>
2024-06-19 14:05:47 -0300

    Update and rename Produto.java to Main.java

--- Main.java
+++ Main.java
@@ -2,30 +2,6 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
-class Produto {
-    private String codigoDeBarras;
-    private String nome;
-    private String ingredientes;
-
-    public Produto(String codigoDeBarras, String nome, String ingredientes) {
-        this.codigoDeBarras = codigoDeBarras;
-        this.nome = nome;
-        this.ingredientes = ingredientes;
-    }
-
-    public String getCodigoDeBarras() {
-        return codigoDeBarras;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public String getIngredientes() {
-        return ingredientes;
-    }
-}
-
 class Sistema {
     private Map<String, Produto> bancoDeProdutos;
 
